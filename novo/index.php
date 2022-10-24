2d025a3d36f9a148c79dc292c2c80950ab16f388
Fernando Henrique <89784479+FerHe05@users.noreply.github.com>
2022-10-24 11:38:08 -0300

    Add files via upload

---
+++ novo/index.php
@@ -0,0 +1,42 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+ </head>
+ <body>
+    <header>
+        <div class="center"></div><!--center-->
+            <div class= "menu"><!--menu-->
+            <a href="home.php">Home</a>
+            <a href="about.php">Profile</a>
+            <a href="home.php">About</a>
+            <a href="home.php">Help</a>
+
+            </div><!--menu-->
+        </div><!--center-->
+    </header>
+    <section class="sobre">
+        <div class="extras">
+        </div>
+        <div class="center">
+            <div class="texto">
+                <h1>In vino veritas
+                <br/>
+                 Carpe diem. 
+                </h1> 
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                    Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa.
+                     Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                     Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.</p>
+            <button>Learn More</button>
+            </div>
+        </div>
+    </section>
+ </body>
+ </html>
